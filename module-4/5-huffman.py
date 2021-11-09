7cd9ae52ab3426aab9239a7b5dd8bb239c1c2007
sesp13 <sespinosarte99@gmail.com>
2021-11-09 11:50:30 -0500

    -add desk test to module 4 ex 3

--- module-4/5-huffman.py
+++ module-4/5-huffman.py
@@ -1,5 +1,24 @@
-def compressAlphabet(alphabetArr):
-    print(alphabetArr)
+import heapq
+
+
+def compressAlphabet(numberOfChars, alphabetArr):
+    # Sort array by frecuency
+    # alphabetArr = sorted(alphabetArr, key=lambda x: x[1])
+
+    # Priority queque
+    queue = []
+    heapq.heapify(queue)
+
+    for i in range(numberOfChars):
+        element = alphabetArr[i]
+        node = {}
+        node['char'] = element[0]
+        node['freq'] = element[1]
+        node['left'] = None
+        node['right'] = None
+        heapq.heappush(queue, node)
+
+    print(queue)
 
 
 def main():
@@ -13,10 +32,10 @@ def main():
             arr[1] = int(arr[1])
             caseArr.append(arr)
 
-        finalArray.append(caseArr)
+        finalArray.append([numberOfChars, caseArr])
 
-    for alphabetArr in finalArray:
-        compressAlphabet(alphabetArr)
+    for alphabetItem in finalArray:
+        compressAlphabet(alphabetItem[0], alphabetItem[1])
 
 
 main()
