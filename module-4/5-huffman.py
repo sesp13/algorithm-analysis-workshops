1b463016ce59da442f40fbfe43b6abca73fbcc75
sesp13 <sespinosarte99@gmail.com>
2021-11-09 23:25:42 -0500

    module 4: ex 5 huffman first full version

--- module-4/5-huffman.py
+++ module-4/5-huffman.py
@@ -1,6 +1,26 @@
 import heapq
 
 
+def iterateTree(digit='', structure={}):
+    if(structure == -1):
+        return
+
+    digit += structure['digit']
+    leftNode = structure['left']
+    rightNode = structure['right']
+
+    if(leftNode == -1):
+        print("{} {}".format(structure['char'], digit))
+    else:
+        iterateTree(digit, leftNode)
+
+    # Only check right node if leftNode
+    if(rightNode == -1 and leftNode != -1):
+        print("{} {}".format(structure['char'], digit))
+    else:
+        iterateTree(digit, rightNode)
+
+
 def compressAlphabet(numberOfChars, alphabetArr):
     # Sort array by frecuency
     # alphabetArr = sorted(alphabetArr, key=lambda x: x[1])
@@ -9,16 +29,47 @@ def compressAlphabet(numberOfChars, alphabetArr):
     queue = []
     heapq.heapify(queue)
 
-    for i in range(numberOfChars):
-        element = alphabetArr[i]
-        node = {}
-        node['char'] = element[0]
-        node['freq'] = element[1]
-        node['left'] = None
-        node['right'] = None
+    # Create all nodes for the tree
+    for letter in alphabetArr:
+        node = [-1, -1]
+        dictObject = {}
+        dictObject['char'] = letter[0]
+        dictObject['left'] = -1
+        dictObject['right'] = -1
+
+        # Build Node
+        node[0] = letter[1]
+        node[1] = dictObject
+
+        # Add node to the queue
         heapq.heappush(queue, node)
 
-    print(queue)
+    # Begin the filter of the tree
+    for _ in range(numberOfChars - 1):
+        node = [-1, -1]
+        dictObject = {}
+        nodeLeft = heapq.heappop(queue)
+        nodeRight = heapq.heappop(queue)
+        nodeLeft[1]['digit'] = '0'
+        nodeRight[1]['digit'] = '1'
+        dictObject['left'] = nodeLeft[1]
+        dictObject['right'] = nodeRight[1]
+        dictObject['char'] = dictObject['left']['char'] + \
+            dictObject['right']['char']
+        newFrecuency = nodeLeft[0] + nodeRight[0]
+
+        # Build new node
+        node[0] = newFrecuency
+        node[1] = dictObject
+
+        # Add node to the queue
+        heapq.heappush(queue, node)
+
+    # Get decision tree
+    tree = heapq.heappop(queue)[1]
+
+    iterateTree('', tree['left'])
+    iterateTree('', tree['right'])
 
 
 def main():
@@ -34,8 +85,11 @@ def main():
 
         finalArray.append([numberOfChars, caseArr])
 
-    for alphabetItem in finalArray:
+    for i in range(len(finalArray)):
+        print("caso {}:".format(i + 1))
+        alphabetItem = finalArray[i]
         compressAlphabet(alphabetItem[0], alphabetItem[1])
+        print("")
 
 
 main()
