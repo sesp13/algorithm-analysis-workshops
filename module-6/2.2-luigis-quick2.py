fb0a31325f23f33bc295176a21987918d00ecc9b
sesp13 <sespinosarte99@gmail.com>
2021-11-20 01:35:32 -0500

    add 2 versions of luigis family using quick sort

--- module-6/2.2-luigis-quick2.py
+++ module-6/2.2-luigis-quick2.py
@@ -1,6 +1,9 @@
 from math import floor
 from random import randint
 
+desiredIndex = 0
+desiredContent = -1
+
 
 def getPivotIndex(arr, a, b, p):
     xa = arr[a]
@@ -25,32 +28,44 @@ def getPivotIndex(arr, a, b, p):
 
 
 def quickSort(arr, a, b):
+    global desiredContent
     if b - a > 0:
+        # Randomly choose a pivot
         p = randint(a, b)
         pivotCorrectIndex = getPivotIndex(arr, a, b, p)
-        quickSort(arr, a, pivotCorrectIndex - 1)
-        quickSort(arr, pivotCorrectIndex + 1, b)
+        if(pivotCorrectIndex == desiredIndex):
+            # Stop recursion when the desired pivot is found
+            desiredContent = arr[pivotCorrectIndex]
+        else:
+            quickSort(arr, a, pivotCorrectIndex - 1)
+            quickSort(arr, pivotCorrectIndex + 1, b)
     return arr
 
 
 def getBestHouse(arr):
+    global desiredIndex
+    global desiredContent
+
     # Sort array
     arrLength = len(arr)
     middle = floor(arrLength / 2)
 
     # Get correct index
     index = middle - 1 if arrLength % 2 == 0 else middle
+    desiredIndex = index
 
+    # Find desired content using quick sort
     arr = quickSort(arr, 0, arrLength - 1)
 
-    selectedHouse = arr[index]
+    desiredContent = arr[desiredIndex] if(
+        desiredContent == -1) else desiredContent
 
     diff = 0
     for element in arr:
-        if(element != selectedHouse):
-            diff += abs(selectedHouse - element)
+        if(element != desiredContent):
+            diff += abs(desiredContent - element)
 
-    print("{} {}".format(selectedHouse, diff))
+    print("{} {}".format(desiredContent, diff))
 
 
 def main():
