e5f1e0c02d3a49067944f3d15631bef8d881cbab
sesp13 <sespinosarte99@gmail.com>
2021-11-28 18:00:45 -0500

    module7 ex 3 karatsuba v1.0

---
+++ module-7/3-karatsuba.py
@@ -0,0 +1,47 @@
+import math
+
+
+def getKaratsuba(n, number1, number2):
+    if(n == 1):
+        resultNumber = int(number1) * int(number2)
+        print(resultNumber)
+        return resultNumber
+    else:
+        half = int(n / 2)
+        a = number1[:half]
+        b = number1[half:]
+        c = number2[:half]
+        d = number2[half:]
+
+        P1 = getKaratsuba(half, a, c)
+        P2 = getKaratsuba(half, a, d)
+        P3 = getKaratsuba(half, b, c)
+        P4 = getKaratsuba(half, b, d)
+
+        resultNumber = (math.pow(10, n) * P1) + \
+            (math.pow(10, half) * (P2 + P3)) + P4
+
+        resultNumber = int(resultNumber)
+        print(resultNumber)
+        return resultNumber
+
+
+def main():
+    cases = int(input())
+    finalArr = []
+    caseCount = 1
+    for _ in range(cases):
+        arr = [x for x in input().split()]
+        finalArr.append([caseCount, arr])
+        caseCount += 1
+
+    for item in finalArr:
+        print("caso {}:".format(item[0]))
+        n = int(item[1][0])
+        n1 = item[1][1]
+        n2 = item[1][2]
+        getKaratsuba(n, n1, n2)
+        print("")
+
+
+main()
