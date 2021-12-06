fdabf20188014f528efed98f987756ac3d7f54c2
sesp13 <sespinosarte99@gmail.com>
2021-12-05 22:38:57 -0500

    module 8 ex 2 v3.0

--- module-8/2-candy-cut.py
+++ module-8/2-candy-cut.py
@@ -1,32 +1,34 @@
-def getMinMaxCosts(caseArr):
-    arrLength = caseArr[0]
-    p = caseArr[1:]
-    r = [[0, 0] for _ in range(arrLength)]
-    # Min 0 Max 1
-    r[0] = [p[0], p[0]]
-    for i in range(1, arrLength):
-        # Set current price for the index
-        # Min 0 Max 1
-        r[i] = [p[i], p[i]]
-        # Calculate rest
-        for j in range(i):
-            # Get current price for the index
-            pj = p[j]
-            # Get the index of the rest pending
-            restIndex = i - j - 1
-            # Fill for the min proccess
-            r[i][0] = min(r[i][0], pj + r[restIndex][0])
-            # Fill for the max procceses
-            r[i][1] = max(r[i][1], pj + r[restIndex][1])
-
-    lastIndex = arrLength - 1
-    print("{} {}".format(r[lastIndex][0], r[lastIndex][1]))
+from sys import stdin, stdout
 
 
 def main():
-    cases = int(input())
+    cases = int(stdin.readline())
     for _ in range(cases):
-        getMinMaxCosts([int(x) for x in input().split()])
+        caseArr = [int(x) for x in stdin.readline().split()]
+        arrLength = caseArr[0]
+        p = caseArr[1:]
+        r1 = [0 for __ in range(arrLength)]
+        r2 = [0 for __ in range(arrLength)]
+        r1[0] = p[0]
+        r2[0] = p[0]
+        for i in range(1, arrLength):
+            # Set current price for the index
+            r1[i] = p[i]
+            r2[i] = p[i]
+            # Calculate rest
+            for j in range(i):
+                # Get current price for the index
+                pj = p[j]
+                # Get the index of the rest pending
+                restIndex = i - j - 1
+                # Fill for the min proccess
+                r1[i] = min(r1[i], pj + r1[restIndex])
+                # Fill for the max procceses
+                r2[i] = max(r2[i], pj + r2[restIndex])
+
+        lastIndex = arrLength - 1
+        stdout.write("{} {}\n".format(
+            str(r1[lastIndex]), str(r2[lastIndex])))
 
 
 main()
