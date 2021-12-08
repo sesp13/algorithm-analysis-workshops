6fd9195591cebe28e5ff4e9c32ab04abb49f89d3
sesp13 <sespinosarte99@gmail.com>
2021-12-08 02:25:17 -0500

    module 8 ex 3 pascals preversion with final results

--- module-8/3-pascal.py
+++ module-8/3-pascal.py
@@ -1,48 +1,55 @@
 import math
 
 GLOBALMOD = 999999937
-
 oneMod = 1 % GLOBALMOD
-
 globalPascalArr = [[oneMod], [oneMod, oneMod]]
-# globalPascalArr = [[1], [1, 1]]
+globalMaxArr = [oneMod, oneMod]
+maxLevel = 0
 
 
-def buildPascal(maxLevel):
+def buildPascal():
+    global maxLevel
     if(maxLevel <= 2):
         return
     else:
         global globalPascalArr
+        global globalMaxArr
         global GLOBALMOD
         global oneMod
         for level in range(1, maxLevel - 1):
             nextLevelArr = [oneMod]
             currentLevelArr = globalPascalArr[level]
             # Build new level
+            maxSum = 0
             for i in range(1, len(currentLevelArr)):
                 prevIndex = i - 1
                 sum = currentLevelArr[prevIndex] + currentLevelArr[i]
+                maxSum = sum if(sum > maxSum) else maxSum
                 nextLevelArr.append(sum % GLOBALMOD)
 
             # Add final element
             nextLevelArr.append(oneMod)
-
+            # Add best sum
+            globalMaxArr.append(maxSum)
+            # Add new level to pascal
             globalPascalArr.append(nextLevelArr)
 
 
 def getPascals(arr: list):
-    global globalPascalArr
-    maxLevel = max(arr)
-    buildPascal(maxLevel)
-    print(3 % GLOBALMOD)
-    print("Hello world")
-
+    global globalMaxArr
+    global maxLevel
+    buildPascal()
+    for level in arr:
+        print(globalMaxArr[level - 1])
 
 def main():
+    global maxLevel
     cases = int(input())
     finalArr = []
     for _ in range(cases):
-        finalArr.append(int(input()))
+        level = int(input())
+        maxLevel = level if(level > maxLevel) else maxLevel
+        finalArr.append(level)
 
     getPascals(finalArr)
 
