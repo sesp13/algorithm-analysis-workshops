c9134d41553760e7f3664821cb204c52eaef35e2
sesp13 <sespinosarte99@gmail.com>
2021-12-23 01:09:16 -0500

    module 9 ex 3 hucks journey first approach

---
+++ module-9/3-huck-journey.py
@@ -0,0 +1,42 @@
+import math
+
+
+def getMinCost(costArr: list):
+    N = len(costArr)
+    M = [[math.inf for __ in range(N)] for _ in range(N)]
+    for steps in range(N):
+        for i in range(N - steps):
+            j = i + steps
+            lowest = costArr[i][j]
+            for k in range(i + 1, j - 1):
+                lowest = min(lowest, M[k][j] + M[i][k])
+            M[i][j] = lowest
+
+    print(M)
+
+
+def main():
+    cases = int(input())
+    finalArr = []
+    for _ in range(cases):
+        parades = int(input())
+        caseArr = []
+        for i in range(parades):
+            currentRow = [x for x in input().split()]
+            # Preprocess matrix
+            for j in range(parades):
+                if(i == j):
+                    currentRow[j] = 0
+                elif (currentRow[j] == '*'):
+                    currentRow[j] = math.inf
+                else:
+                    currentRow[j] = int(currentRow[j])
+            caseArr.append(currentRow)
+
+        finalArr.append(caseArr)
+
+    for arr in finalArr:
+        getMinCost(arr)
+
+
+main()
