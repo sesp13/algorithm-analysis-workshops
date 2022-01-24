581034eb53cf5a065fe2f997c587b0ed586caa40
sesp13 <sespinosarte99@gmail.com>
2022-01-23 23:33:43 -0500

    model 10 ex 5 add recursion function

--- module-10/5-christmas.py
+++ module-10/5-christmas.py
@@ -1,4 +1,15 @@
 import math
+S = []
+p = []
+
+
+def buildTree(i, j, level: int = 0):
+    root = S[i][j]
+    if root < j:
+        buildTree(root + 1, j, level + 1)
+        print('\t'*level + p[root][0])
+    else:
+        buildTree(i, root - 1, level + 1)
 
 
 def getSum(p):
@@ -8,9 +19,12 @@ def getSum(p):
     return s
 
 
-def getTree(p: list):
+def getTree():
+    global p
+    global S
     arrLength = len(p)
     C = [[[0, 0] for __ in range(arrLength)] for _ in range(arrLength)]
+    S = [[0 for __ in range(arrLength)] for _ in range(arrLength)]
 
     for i in range(arrLength):
         C[i][i] = p[i]
@@ -24,14 +38,19 @@ def getTree(p: list):
                 r2 = r + 1
                 item1 = 0 if r1 < 0 else C[i][r1][1]
                 item2 = 0 if r2 >= arrLength else C[r2][j][1]
-                lowest = min(lowest, item1 + item2 + getSum(p[i:j+1]))
+                expr = item1 + item2 + getSum(p[i:j+1])
+                if(expr < lowest):
+                    lowest = expr
+                    S[i][j] = r
 
             C[i][j] = [0, lowest]
 
-    print(C[0][-1])
+    buildTree(0, arrLength - 1)
 
 
 def main():
+    global p
+
     cases = int(input())
     finalArr = []
     for _ in range(cases):
@@ -45,7 +64,8 @@ def main():
     finalIndex = len(finalArr) - 1
     for i in range(len(finalArr)):
         print(f"caso {i + 1}:")
-        getTree(finalArr[i])
+        p = finalArr[i]
+        getTree()
         if(i != finalIndex):
             print('')
 
