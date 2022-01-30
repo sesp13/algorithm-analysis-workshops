1b211eb99ccc3fb912abe52f861a23f6ec49418d
sesp13 <sespinosarte99@gmail.com>
2022-01-30 15:32:42 -0500

    module 11 ex 2 v1.0

--- module-11/2-game-of-thrones.py
+++ module-11/2-game-of-thrones.py
@@ -1,19 +1,34 @@
-def getInfo(arr: list):
-    families = []
-    for register in arr:
-        familyFound = False
-        for family in families:
-            isRegister1 = register[0] in family
-            isRegister2 = register[1] in family
-            if(isRegister1 or isRegister2):
-                familyFound = True
-                if(not isRegister1):
-                    family.append(register[0])
-                if(not isRegister2):
-                    family.append(register[1])
+currentGraph = {}
+currentFamily = []
 
-        if(not familyFound):
-            families.append(register)
+
+def checkMembers(key):
+    global currentGraph
+    global currentFamily
+
+    if(currentGraph[key]['explored'] == True):
+        return
+    else:
+        currentGraph[key]['explored'] = True
+        if (key not in currentFamily):
+            currentFamily.append(key)
+        # Go on with the related
+        for newNode in currentGraph[key]['related']:
+            checkMembers(newNode)
+
+
+def getInfo(graph: object):
+    global currentGraph
+    global currentFamily
+
+    currentGraph = graph
+    families = []
+    for key in graph:
+        currentNode = graph[key]
+        if(currentNode['explored'] == False):
+            currentFamily = []
+            checkMembers(key)
+            families.append(currentFamily)
 
     maxNumber = 0
     for family in families:
@@ -27,13 +42,37 @@ def main():
     finalArr = []
     for _ in range(cases):
         R = int(input())
-        caseArr = []
+        graph = {}
         for __ in range(R):
-            caseArr.append([x for x in input().split()])
-        finalArr.append(caseArr)
+            register = [x for x in input().split()]
 
-    for arr in finalArr:
-        getInfo(arr)
+            # Add new nodes to the graph
+            if(graph.get(register[0]) == None):
+                graph[register[0]] = {
+                    "explored": False,
+                    "related": []
+                }
+
+            if(graph.get(register[1]) == None):
+                graph[register[1]] = {
+                    "explored": False,
+                    "related": []
+                }
+
+            # Update related nodes in the graph
+            related1 = graph[register[0]]['related']
+            related2 = graph[register[1]]['related']
+            
+            if(register[1] not in related1):
+                related1.append(register[1])
+
+            if(register[0] not in related2):
+                related2.append(register[0])
+
+        finalArr.append(graph)
+
+    for graph in finalArr:
+        getInfo(graph)
 
 
 main()
